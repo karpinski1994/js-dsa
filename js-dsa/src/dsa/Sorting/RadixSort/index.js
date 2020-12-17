cb8c49c4f8e48fea71fa431d2fe023f897d464ee
Patryk Karpinski <Patryk.Karpinski@igt.com>
2020-12-17 21:52:03 +0000

    Add radixSort

--- js-dsa/src/dsa/Sorting/RadixSort/index.js
+++ js-dsa/src/dsa/Sorting/RadixSort/index.js
@@ -2,7 +2,7 @@ const getDigit = (num, i) => Math.floor(Math.abs(num) / Math.pow(10, i)) % 10;
 
 const digitCount = (num) => {
   if (num === 0) return 1;
-  return Math.floor(Math.long10(Math.abs(num))) + 1
+  return Math.floor(Math.long10(Math.abs(num))) + 10
 }
 
 const mostDigits = (nums) => {
@@ -12,3 +12,18 @@ const mostDigits = (nums) => {
   }
   return maxDigits
 }
+
+
+const radixSort = (nums)=> {
+  let maxDigit = mostDigits(nums);
+  for(let k = 0; k < maxDigit; k++) {
+    let digitbuckets = Array.from({length: 10}, () => [])
+    for(let i = 0; i < nums.length; i ++) { //iteruiemy po wszystkich liczbach!
+      let digit = getDigit(nums[i], k);
+      digitbuckets[digit].push(nums[i])
+    }
+    nums = [].concat(...digitbuckets)
+  }
+  return nums
+}
+
